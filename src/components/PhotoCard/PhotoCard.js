5a570120794b5740a03e8189ed20984643397ea2
lmarello <leomarello@gmail.com>
2021-03-04 16:01:33 -0300

    photoCard IntersectionObserver and save like in storage

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,22 +1,66 @@
-import React from 'react'
-import { ImgWrapper, Img, Button } from './PhotoCard.styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import React, { useEffect, useRef, useState } from 'react'
+import { ImgWrapper, Img, Button, Article } from './PhotoCard.styles'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 
 const TMP =
   'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
 
-export const PhotoCard = ({ id, likes = 0, src = TMP }) => {
+export const PhotoCard = ({ id, src = TMP, likes = 0 }) => {
+  const ref = useRef(null)
+  const [show, setShow] = useState(false)
+  const localStorageKey = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      return window.localStorage.getItem(localStorageKey)
+    } catch (error) {
+      console.log(error)
+      return false
+    }
+  })
+
+  useEffect(() => {
+    Promise.resolve(
+      typeof (window.IntersectionObserver !== 'undefined')
+        ? window.IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0]
+        if (isIntersecting) {
+          setShow(true)
+          observer.disconnect()
+        }
+      })
+      observer.observe(ref.current)
+    })
+  }, [ref])
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
+
+  const setLocalStorage = (key, value) => {
+    try {
+      window.localStorage.setItem(key, value)
+      setLiked(value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} />
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size={24} />
-        {likes} likes!
-      </Button>
-    </article>
+    <Article ref={ref}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} />
+            </ImgWrapper>
+          </a>
+          <Button onClick={() => setLocalStorage(localStorageKey, !liked)}>
+            <Icon size={24} />
+            {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
   )
 }
