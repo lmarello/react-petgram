2404fd940a1b5fc866bed3199fb5d6bdd695d4dc
lmarello <leomarello@gmail.com>
2021-03-04 18:02:12 -0300

    useNearScreen hook

--- src/components/PhotoCard/PhotoCard.js
+++ src/components/PhotoCard/PhotoCard.js
@@ -1,38 +1,20 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { ImgWrapper, Img, Button, Article } from './PhotoCard.styles'
 import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
 import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
-const TMP =
-  'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png'
+const NO_IMAGE =
+  'https://res.cloudinary.com/devixuds6/image/upload/v1614891644/petgram/no-image_ovsn7p.png'
 
-export const PhotoCard = ({ id, src = TMP, likes = 0 }) => {
-  const ref = useRef(null)
-  const [show, setShow] = useState(false)
+export const PhotoCard = ({ id, src = NO_IMAGE, likes = 0 }) => {
   const localStorageKey = `like-${id}`
   const [liked, setLiked] = useLocalStorage(localStorageKey, false)
-
-  useEffect(() => {
-    Promise.resolve(
-      typeof (window.IntersectionObserver !== 'undefined')
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(() => {
-      const observer = new window.IntersectionObserver(function (entries) {
-        const { isIntersecting } = entries[0]
-        if (isIntersecting) {
-          setShow(true)
-          observer.disconnect()
-        }
-      })
-      observer.observe(ref.current)
-    })
-  }, [ref])
-
+  const [show, element] = useNearScreen(false)
   const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
-    <Article ref={ref}>
+    <Article ref={element}>
       {show && (
         <>
           <a href={`/detail/${id}`}>
