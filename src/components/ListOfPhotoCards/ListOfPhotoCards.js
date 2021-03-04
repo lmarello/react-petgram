458555311a95ab4fa1aedca35984356317b8117c
lmarello <leomarello@gmail.com>
2021-03-04 15:57:15 -0300

    listOfPhotoCards add styled-component

--- src/components/ListOfPhotoCards/ListOfPhotoCards.js
+++ src/components/ListOfPhotoCards/ListOfPhotoCards.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { PhotoCard } from '../PhotoCard'
+import { List } from './ListOfPhotoCards.styles'
 
 export const ListOfPhotoCards = () => {
   return (
-    <ul>
-      <li>
-        {[1, 2, 3].map((photoCard) => (
-          <PhotoCard key={photoCard} />
-        ))}
-      </li>
-    </ul>
+    <List>
+      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((photoCard) => (
+        <li key={photoCard}>
+          <PhotoCard id={photoCard} />
+        </li>
+      ))}
+    </List>
   )
 }
---
+++ src/components/ListOfPhotoCards/ListOfPhotoCards.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const List = styled.ul`
+  width: 100%;
+  padding: 0;
+  margin-top: 20px;
+`
