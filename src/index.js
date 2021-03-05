3e670145ea0b4d3ef744c63878f926d2283590ca
lmarello <leomarello@gmail.com>
2021-03-05 00:51:39 -0300

    add apollo provider index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
 import { App } from './App'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const client = new ApolloClient({
+  uri: 'https://petgram-server-jrmfsd-okxluew9o.now.sh/graphql',
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+  //eslint-disable-next-line react/jsx-filename-extension
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('app')
+)
